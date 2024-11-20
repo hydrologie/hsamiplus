b506f56644dbc4ec5346b1782743375b7e50d5cd
dhagum <haguma.didier@hydroquebec.com>
2024-11-20 10:17:35 -0500

    noyau

---
+++ src/hsamiplus/hsami_ecoulement_horizontal.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+import numpy as np
+
+
+def hsami_ecoulement_horizontal(nb_pas,vidange_reserve_inter,reserve_inter,eau_hydrogrammes,
+                                hydrogrammes,apport_vertical,modules):
+    '''
+     ENTRÉES
+     -------
+     nb_pas                : Voir hsami2_noyau (projet['nb_pas_par_jour'])
+     vidange_reserve_inter : Voir hsami2_noyau (projet['param(19)']) 
+     reserve_inter         : Voir hsami2_noyau (etat['reserve'])
+     eau_hydrogrammes      : Voir hsami2_noyau (etat['eau_hydrogrammes'])
+     hydrogrammes          : Hydrogrammes unitaires de surface et 
+                             intermediaire
+     apport_vertical       : Lames d'eau verticales (voir hsami_interception)
+     modules               : Voir hsami2_noyau (projet['modules'])
+
+     SORTIES
+     -------
+     apport                : Apports verticaux laminés
+     reserve_inter         : Voir hsami2_noyau (etat['reserve'])
+     eau_hydrogrammes      : Voir hsami2_noyau (etat['eau_hydrogrammes'])
+
+     EXEMPLE
+     -------
+     Pas de temps 120 de projet_test.json, tous les modules sont aux valeurs
+     PAR DÉFAUT
+
+    '''
+    
+    # On pondere le taux de vidange en fonction du pas de temps
+    vidange_reserve_inter = 1 - (1-vidange_reserve_inter)/nb_pas
+
+    # Distribution du ruissellement de surface en fonction de l'hydrogramme de surface
+    eau_hydrogrammes[:,0] = eau_hydrogrammes[:,0] + hydrogrammes[:,0] * apport_vertical[2]
+    
+    if modules['mhumide'] == 1:
+        eau_hydrogrammes[:,2] = eau_hydrogrammes[:,2] + hydrogrammes[:,0] * apport_vertical[5]
+
+    # Calcul de l'apport latéral
+    apport = [apport_vertical[0], reserve_inter, eau_hydrogrammes[0,0], apport_vertical[3], 
+              apport_vertical[4], eau_hydrogrammes[0,2]]
+    # Ex.: apport = [0.0510, 0.0147, 0.0012, -0.0894, 0, 0]
+
+    # Alimentation de la réserve intermédiaire par le ruissellement intermédiaire
+    eau_hydrogrammes[0,1] = apport_vertical[1]
+    eau_inter = np.sum(eau_hydrogrammes[:,1] * hydrogrammes[:,1])
+    reserve_inter = reserve_inter * vidange_reserve_inter + eau_inter*(1 - vidange_reserve_inter)
+    # Ex.: eau_inter = 0.0060
+
+    # ---------------------------------------
+    # Décalage de l'eau dans les hydrogrammes
+    # ---------------------------------------
+    
+    # Hydrogramme de surface: décalage vers la gauche
+    eau_hydrogrammes[:-1,0] = eau_hydrogrammes[1:,0]
+    eau_hydrogrammes[-1,0]  = 0
+
+    eau_hydrogrammes[:-1,2] = eau_hydrogrammes[1:,2]
+    eau_hydrogrammes[-1,2]  = 0
+
+    # Hydrogramme intermédiaire: décalage vers la droite
+    eau_hydrogrammes[1:,1] = eau_hydrogrammes[:-1,1]
+    eau_hydrogrammes[0,1]  = 0
+
+
+    return apport, reserve_inter, eau_hydrogrammes
+
+
